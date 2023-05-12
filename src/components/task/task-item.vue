dd94309ea9e8a992d579f4caeb458ebc749c19b2
ibra <ibrahimalanshor6@gmail.com>
2023-05-12 14:56:46 +0800

    feat: elemnt

--- src/components/task/task-item.vue
+++ src/components/task/task-item.vue
@@ -34,10 +34,10 @@ function handleEdit() {
 <template>
   <div class="relative flex items-start">
     <div class="flex h-6 items-center">
-      <input :id="`task-${props.task.id}`" aria-describedby="comments-description" name="comments" type="checkbox" class="h-4 w-4 rounded-full border-gray-300 text-blue-600 focus:ring-blue-600" v-model="isDone" />
+      <input aria-describedby="comments-description" name="comments" type="checkbox" class="h-4 w-4 rounded-full border-gray-300 text-blue-600 focus:ring-blue-600" v-model="isDone" />
     </div>
     <div class="ml-3 text-sm leading-6 flex items-center gap-x-2 group">
-      <label :for="`task-${props.task.id}`" class="font-medium text-gray-900" :class="{ 'line-through': props.task.done }">{{ props.task.name }}</label>
+      <label class="font-medium text-gray-900" :class="{ 'line-through': props.task.done }">{{ props.task.name }}</label>
       <button class="hidden group-hover:block" v-on:click="handleEdit">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-blue-600">
           <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
